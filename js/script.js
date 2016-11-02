6e12c5df25f6c6da3f1b4b88ce9ac18a247c41e4
noahbaker <nbaker554@gmail.com>
2016-11-02 03:20:07 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+	$('#box').click('click touchstart', function() {
+	$( this ).css( "width", "20%" );
+	
+	});
+
+// $("html, body").animate({ scrollTop: 0 }, "slow");
+
+
+});
+
